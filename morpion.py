99204e0b5146c6b73dd02133553f681b299abd61
matheobechet <82206819+matheobechet@users.noreply.github.com>
2021-04-28 11:59:42 +0200

    Add files via upload

---
+++ morpion.py
@@ -0,0 +1,110 @@
+import pygame
+import sys
+class Grille:
+
+    def __init__(self,ecran):
+        """
+        initie la grille
+        :param: ecran
+        """
+        self.ecran = ecran
+        self.lignes = [(( 200,0),(200,600))
+        ((400,0),(400,600))
+        ((0,200),(600,200))
+        ((0,400),(600,400)),]
+        # initer la grille
+        self.grille =[[None for x in range (0,3)] for y in range (0,3)]
+        #initier une variable pour verifier si le compteur est 'ON'
+        self.compteur_on = False
+    def afficher(self):
+        """
+        Affiche la grille et les X/0
+        """
+
+        for ligne in self.ligne_:
+            pygame.draw.line(self.ecran,(0,0,0),ligne[0],ligne[1],2)
+        #afficher les X et O
+        for y in range(0,len(self.grille)):
+            for x in range(0,len(self.grille)):
+                if self.grille[y][x] == 'X':
+                    pygame.draw.line(self.ecran, (0, 0, 0), (x * 200, y* 200), (200 + (x * 200), 200 + (y *200)), 3)
+                    pygame.draw.line(self.ecran, (0, 0, 0), ((x * 200), 200 + (y * 200)), (200 + (x * 200), (y * 200)), 3)
+                
+                elif self.grille[y][x] == '0':
+                    
+                    pygame.draw.circle(self.ecran, (0, 0, 0), (100 + (x * 200), 100 + (y * 200)), 100, 3)
+                    
+    def print_grille(self):
+        """
+        Afficher la grille dans la console
+        """
+        print(self.grille)
+    def fixer_la_valeur(self,x,y,valeur):
+        """
+        Fixe la valeur d'une case dans la grille
+        
+        :param: x
+        :param: y
+        :param : valeur
+        """
+        # cond si une case possede la valeur None
+        if self.grille[y][x] == None :
+            self.grille[y][x] = valeur
+        #le compteur est ON
+        self.compteur_on = True
+
+
+class Jeu :
+
+    def __init__(self):
+        self.ecran = pygame.display.set_mode((600,600))
+        pygame.dipslay.set_caption('Tic Tac Toe')
+        self.jeu_encours = True
+        self.grille = Grille(self.ecran)
+        #fixer les variables 'X' et 'O'
+        self.player_X = 'X'
+        self.player_O = 'O'
+        
+        #fixer le compteur
+        self.compteur = 0
+    
+    def fonction_principale(self):
+        while self.jeu_encours:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                # ajouter l'evenement qui correspond au clique droit
+                if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+                    # obtenir la position de la souris
+                    position = pygame.mouse.get_pos()
+                    print(position)
+                    position_x , position_y = position[0]//200 ,position[1]//200
+                    print(position_x,position_y)
+                    
+        
+                    # cond si le compteur est pair ou impair
+                    print(self.compteur,self.compteur%2)
+                    if self.compteur %2 == 0 :
+                        self.grille.fixer_la_valeur(position_x,position_y,self.player_X)
+                    else:
+                        self.grille.fixer_la_valeur(position_x,position_y,self.player_O)
+                    #cond si le compteur ON est vrai
+                    if self.grille.compteur_on:
+                        self.compteur += 1
+                    #fixe le compteur_ON = Faux
+                        self.grille.compteur_on = False
+
+
+                self.grille.print_grille()
+
+
+
+            self.ecran.fill((240,240,240))
+            self.grille.afficher()
+            pygame.display.flip()
+
+
+if __name__ == ' main':
+    pygame.init()
+    Jeu().fonction_principale()
+    pygame.quit()
